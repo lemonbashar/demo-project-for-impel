a5cc4585c7abc645c7e58f563f8e2bafe5ae4ba4
lemonbashar <lemon.bashar@gmail.com>
2019-10-25 12:15:56 +0600

    Init Project

---
+++ src/main/java/com/demoforimpel/DemoForImpelApplication.java
@@ -0,0 +1,13 @@
+package com.demoforimpel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForImpelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForImpelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoforimpel/DemoForImpelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoforimpel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForImpelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
